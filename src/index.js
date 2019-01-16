7a77b79284234e710c8af345e1eee657afa15c31
Amin Zibayi <7Developer2@gmail.com>
2019-01-16 20:47:58 +0330

    Update .gitignore

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* In the Name of Allah */
+
+import Provider from './Provider';
+import Extension from './Extension';
+import Extensions from './Extensions';
+import Actions from './Actions';
+
+export {
+  Provider,
+  Extension,
+  Extensions,
+  Actions
+};
